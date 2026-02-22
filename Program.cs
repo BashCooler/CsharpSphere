635d6040832e4a379d18efd62afcc19ae68d82c3
BashCooler <den.tryaskin@mail.ru>
2026-02-22 22:30:39 +0500

    Initial

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.OpenGL.Extensions.ImGui;
+using Silk.NET.Windowing;
+
+namespace CSharpSphere;
+
+public static partial class Program
+{
+    private static GL _gl = null!;
+    private static IWindow _window = null!;
+    private static IInputContext _input = null!;
+    private static ImGuiController _controller = null!;
+    
+    private static readonly Sphere Sphere = new();
+    
+    public static void Main()
+    {
+        var options = WindowOptions.Default with
+        {
+            Title = "Silk.NET Sphere",
+            Size = new Vector2D<int>(800, 600),
+        };
+        _window = Window.Create(options);
+        _window.Load += OnLoad;
+        _window.Update += OnUpdate;
+        _window.Render += OnRender;
+        _window.FramebufferResize += s => _gl.Viewport(s);
+        _window.Closing += OnClose;
+        _window.Run();
+        _window.Dispose();
+    }
+
+    private static void OnLoad()
+    {
+        _gl = _window.CreateOpenGL();
+        _input = _window.CreateInput();
+        _controller = new ImGuiController(_gl, _window, _input);
+    }
+
+    private static void OnUpdate(double deltaTime) { }
+
+    private static void OnRender(double deltaTime)
+    {
+        _gl.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        _gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        
+        RenderUI(deltaTime);
+    }
+
+    private static void OnClose()
+    {
+        _controller.Dispose();
+        _input.Dispose();
+        _gl.Dispose();
+    }
+}

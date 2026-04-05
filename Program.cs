2481c942e1e142b01e242b24f132bee800a0d832
BashCooler <den.tryaskin@mail.ru>
2026-04-06 00:13:30 +0500

    Change axes Z index

--- Program.cs
+++ Program.cs
@@ -60,22 +60,22 @@ public static class Program
     {
         _gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
         _gl.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        DrawAxesXy();
         Sphere.Draw();
-        DrawAxesZ();
         _ui.RenderUi(deltaTime);
     }
 
     public static void DrawLines(Triangle[] triangles, Vector4[,] points)
     {
+        DrawAxes(Axes.X | Axes.Y);
+        
         _gl.Begin(GLEnum.Lines);
         _gl.Color3(0.6f, 0.6f, 0.6f);
         
-        foreach (var tri in triangles)
+        foreach (Triangle tri in triangles)
         {
-            var p1 = points[tri.IdxP1.I, tri.IdxP1.J];
-            var p2 = points[tri.IdxP2.I, tri.IdxP2.J];
-            var p3 = points[tri.IdxP3.I, tri.IdxP3.J];
+            Vector4 p1 = points[tri.IdxP1.I, tri.IdxP1.J];
+            Vector4 p2 = points[tri.IdxP2.I, tri.IdxP2.J];
+            Vector4 p3 = points[tri.IdxP3.I, tri.IdxP3.J];
             
             DrawLine(p1, p2);
             DrawLine(p2, p3);
@@ -83,6 +83,8 @@ public static class Program
         }
         
         _gl.End();
+        
+        DrawAxes(Axes.Z);
     }
 
     public static void DrawPolygons(Triangle[] triangles, Vector4[,] points, bool TwoStep = true)
@@ -102,12 +104,17 @@ public static class Program
             
             DrawTriangle(p1, p2, p3);
         }
+        _gl.End();
+        
+        DrawAxes(Axes.All);
 
         if (!TwoStep)
         {
-            _gl.End();
+            // _gl.End();
             return;
         }
+        
+        _gl.Begin(GLEnum.Triangles);
 
         foreach (Triangle tri in triangles)
         {
@@ -139,31 +146,43 @@ public static class Program
         _gl.Vertex2(p2.X / _windowMinSize, p2.Y / _windowMinSize);
     }
 
-    private static void DrawAxesXy()
+    [Flags]
+    private enum Axes
+    {
+        None = 0,
+        X = 1 << 0,     // 1
+        Y = 1 << 1,     // 2
+        Z = 1 << 2,     // 4
+        All = X | Y | Z // 7
+    }
+
+    private static void DrawAxes(Axes axes)
     {
         _gl.LineWidth(3f);
         _gl.Begin(GLEnum.Lines);
-        
-        // X
-        _gl.Color3(0.8f, 0f, 0f);
-        _gl.Vertex2(0, 0);
-        _gl.Vertex2(0.75f, 0);
-        
-        // Y
-        _gl.Color3(0f, 0.8f, 0f);
-        _gl.Vertex2(0, 0);
-        _gl.Vertex2(0, 0.75f);
+
+        if (axes.HasFlag(Axes.X))
+        {
+            _gl.Color3(0.8f, 0f, 0f);
+            _gl.Vertex2(0, 0);
+            _gl.Vertex2(0.75f, 0);
+        }
+
+        if (axes.HasFlag(Axes.Y))
+        {
+            _gl.Color3(0f, 0.8f, 0f);
+            _gl.Vertex2(0, 0);
+            _gl.Vertex2(0, 0.75f);
+        }
         
         _gl.End();
         _gl.LineWidth(1.0f);
-    }
 
-    private static void DrawAxesZ()
-    {
+        if (!axes.HasFlag(Axes.Z)) return;
+        
         _gl.PointSize(5f);
         _gl.Begin(GLEnum.Points);
-        
-        // Z
+            
         _gl.Color3(0f, 0.2f, 0.8f);
         _gl.Vertex3(0, 0, 0);
         

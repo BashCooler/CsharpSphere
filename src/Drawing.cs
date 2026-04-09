624cf78b040ad6f6b84a2b51d216d950bf394788
BashCooler <den.tryaskin@mail.ru>
2026-04-09 19:12:04 +0500

    Move drawing ops to Drawing.cs

---
+++ src/Drawing.cs
@@ -0,0 +1,186 @@
+#pragma warning disable CS0618 // Type or member is obsolete
+
+using Silk.NET.OpenGL.Legacy;
+
+namespace CSharpSphere;
+
+public static partial class Program
+{
+    public static void DrawLines(Triangle[] triangles, Vector3[,] points)
+    {
+        DrawAxes(Axes.X | Axes.Y);
+
+        _gl.Begin(GLEnum.Lines);
+        _gl.Color3(0.6f, 0.6f, 0.6f);
+
+        foreach (Triangle tri in triangles)
+        {
+            Vector3 p1 = tri.GetP1(points);
+            Vector3 p2 = tri.GetP2(points);
+            Vector3 p3 = tri.GetP3(points);
+
+            DrawLine(p1, p2);
+            DrawLine(p2, p3);
+            DrawLine(p3, p1);
+        }
+
+        _gl.End();
+
+        DrawAxes(Axes.Z);
+    }
+
+    public static void DrawPolygons(Triangle[] triangles, Vector3[,] points, bool TwoStep = true)
+    {
+        _gl.Begin(GLEnum.Triangles);
+
+        foreach (Triangle tri in triangles)
+        {
+            if (TwoStep && tri.Front) continue;
+            
+            Vector3 p1 = tri.GetP1(points);
+            Vector3 p2 = tri.GetP2(points);
+            Vector3 p3 = tri.GetP3(points);
+            
+            Color color = tri.Color;
+            _gl.Color3(color.R, color.G, color.B);
+            
+            DrawTriangle(p1, p2, p3);
+        }
+        _gl.End();
+        
+        DrawAxes(Axes.All);
+
+        if (!TwoStep) return;
+        
+        _gl.Begin(GLEnum.Triangles);
+
+        foreach (Triangle tri in triangles)
+        {
+            if (!tri.Front) continue;
+            
+            Vector3 p1 = tri.GetP1(points);
+            Vector3 p2 = tri.GetP2(points);
+            Vector3 p3 = tri.GetP3(points);
+            
+            Color color = tri.Color;
+            _gl.Color3(color.R, color.G, color.B);
+            
+            DrawTriangle(p1, p2, p3);
+        }
+        
+        _gl.End();
+    }
+
+    private static void DrawTriangle(Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        var s = _windowMinSize;
+        _gl.Vertex2(p1.X / s, p1.Y / s);
+        _gl.Vertex2(p2.X / s, p2.Y / s);
+        _gl.Vertex2(p3.X / s, p3.Y / s);
+    }
+
+    private static void DrawLine(Vector3 p1, Vector3 p2)
+    {
+        var s = _windowMinSize;
+        _gl.Vertex2(p1.X / s, p1.Y / s);
+        _gl.Vertex2(p2.X / s, p2.Y / s);
+    }
+    
+    [Flags]
+    private enum Axes
+    {
+        X = 1 << 0,
+        Y = 1 << 1,
+        Z = 1 << 2,
+        All = X | Y | Z
+    }
+
+    private static void DrawAxes(Axes axes)
+    {
+        _gl.LineWidth(3f);
+        
+        if (axes.HasFlag(Axes.X))
+        {
+            _gl.Begin(GLEnum.Lines);
+            
+            _gl.Color3(0.8f, 0f, 0f);
+            _gl.Vertex2(0, 0);
+            _gl.Vertex2(0.75f, 0);
+            
+            _gl.End();
+            DrawLabelX();
+        }
+
+        if (axes.HasFlag(Axes.Y))
+        {
+            _gl.Begin(GLEnum.Lines);
+            
+            _gl.Color3(0f, 0.8f, 0f);
+            _gl.Vertex2(0, 0);
+            _gl.Vertex2(0, 0.75f);
+            
+            _gl.End();
+            
+            DrawLabelY();
+        }
+
+        if (axes.HasFlag(Axes.Z))
+        {
+            _gl.PointSize(5f);
+            _gl.Begin(GLEnum.Points);
+
+            _gl.Color3(0f, 0.2f, 0.8f);
+            _gl.Vertex3(0, 0, 0);
+
+            _gl.End();
+            _gl.PointSize(1f);
+            
+            DrawLabelZ();
+        }
+        
+        _gl.LineWidth(1.0f);
+    }
+
+    private static void DrawLabelX()
+    {
+        float c = 0.75f * _windowMinSize + Font;
+        float s = _windowMinSize;
+        
+        _gl.Begin(GLEnum.Lines);
+        _gl.Vertex2((c - Font * 0.5f) / s, -Font * 1.5f * 0.5f / s);
+        _gl.Vertex2((c + Font * 0.5f) / s,  Font * 1.5f * 0.5f / s);
+        _gl.Vertex2((c - Font * 0.5f) / s,  Font * 1.5f * 0.5f / s);
+        _gl.Vertex2((c + Font * 0.5f) / s, -Font * 1.5f * 0.5f / s);
+        _gl.End();
+    }
+    
+    private static void DrawLabelY()
+    {
+        float c = 0.75f * _windowMinSize + Font * 1.5f;
+        float s = _windowMinSize;
+        
+        _gl.Begin(GLEnum.Lines);
+        _gl.Vertex2((0 - Font * 0.5f) / s, (c + Font * 1.5f * 0.5f) / s);
+        _gl.Vertex2(0, c / _windowMinSize);
+        _gl.Vertex2((0 + Font * 0.5f) / s, (c + Font * 1.5f * 0.5f) / s);
+        _gl.Vertex2(0, c / s);
+        _gl.Vertex2(0, c / s);
+        _gl.Vertex2(0, (c - Font * 1.5f * 0.5f) / s);
+        _gl.End();
+    }
+    
+    private static void DrawLabelZ()
+    {
+        const float c = -Font * 1.5f;
+        float s = _windowMinSize;
+        
+        _gl.Begin(GLEnum.Lines);
+        _gl.Vertex2((c - Font * 0.5f) / s,  Font * 1.5d * 0.5f / s);
+        _gl.Vertex2((c + Font * 0.5f) / s,  Font * 1.5d * 0.5f / s);
+        _gl.Vertex2((c + Font * 0.5f) / s,  Font * 1.5d * 0.5f / s);
+        _gl.Vertex2((c - Font * 0.5f) / s, -Font * 1.5d * 0.5f / s);
+        _gl.Vertex2((c - Font * 0.5f) / s, -Font * 1.5d * 0.5f / s);
+        _gl.Vertex2((c + Font * 0.5f) / s, -Font * 1.5d * 0.5f / s);
+        _gl.End();
+    }
+}
